f5fbd94d34dfd95f77a7403c63ad29d2e3e03914
Chenqi Wang <chenqiw@bu.edu>
2024-05-07 20:06:45 -0400

    data page

--- dataset/load_and_clean.R
+++ dataset/load_and_clean.R
@@ -6,3 +6,22 @@ data_clean <- data%>%
   na.omit(data)
 saveRDS(data_clean, "dataset/cleaned_dataset.rds")
 cleaned_data <- readRDS("dataset/cleaned_dataset.rds")
+
+
+data_um <- read.csv('dataset/employment_state.csv', skip = 5, header = TRUE)
+colnames(data_um)[1] <- "state"
+data_um<- data_um%>%
+  filter(state != "State" & state != "United States")%>%
+  slice(1:(n() - 2))%>%
+  mutate(
+    X2022 = as.numeric(as.character(X2022)),
+    X2021 = as.numeric(as.character(X2021)),
+    X2020 = as.numeric(as.character(X2020))
+  )
+year_columns <- c("X2020", "X2021", "X2022")
+# Keep the relevant columns only
+data_um <- data_um %>%
+  select(state, all_of(year_columns))
+data_um <- data_um %>%
+  mutate(Average = rowMeans(select(., c(X2022, X2021, X2020)), na.rm = TRUE))
+saveRDS(data_um, "dataset/clean_unemployment.rds")
